325094ad4b55ede5bce3ae1069e6f0483cd4f7d8
02KellyV <900@holbertonschool.com>
2019-09-25 17:04:15 +0000

    main.c created

--- main.c
+++ main.c
@@ -6,24 +6,45 @@
  * @argv: Pointer to arguments array
  * Return: 0 on success or 1 on error
  */
+
 int main(int argc, char **argv[])
 {
-/*getting file and args*/
+	FILE *file;
+	char chr;
+	char letter;
+	size_t len;
+	char **opcode;
+	int boole = 0, i = 0;
+
+
+	/*open*/
 	file = fopen(argv[1], "r");
 	if (file == NULL)
 	{
-		printf("Error: Can't open file %s\n", argv[1]);
+		dprintf(2,"Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
-
-	if (ac != 2)
+	/*validate*/
+	if (argc != 2)
 	{
-		printf("USAGE: monty file\n");
+		dprintf(2,"USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
-
-	return (0);
-
-/*getting line and tokenizing*/
-
+	/*get info*/
+	file = fopen(argv[1], "r");
+	boole = (read =  getline(&letter, &len, file);
+        while (boole != -1)
+        {
+		opcode[i] = strdup(line);
+		i++
+                /*argv[1]*/
+        }
+	opcode[i] = NULL;
+	/*print info*/
+	for (i = 0; opcode[i]; i++)
+	{
+		dprintf("line %i: %s", i, opcode[i]);
+	}
+	fclose(file);
+	return (EXIT_SUCCESS);
 }
