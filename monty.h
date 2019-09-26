d66bb5f811f44db4d833a908b29658eeae901c52
rolandoquiroz <rolandoquiroz@gmail.com>
2019-09-26 18:51:38 +0000

    monty.h with pint

--- monty.h
+++ monty.h
@@ -1,13 +1,17 @@
-#ifndef _MONTY_H_
-#define _MONTY_H_
+#ifndef FILE_MONTY
+#define FILE_MONTY
+#define _GNU_SOURCE
 
+/* imports */
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
+#include <ctype.h>
 #include <fcntl.h>
+#include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
@@ -19,9 +23,9 @@
  */
 typedef struct stack_s
 {
-        int n;
-        struct stack_s *prev;
-        struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
 
 /**
@@ -33,9 +37,36 @@ typedef struct stack_s
  */
 typedef struct instruction_s
 {
-        char *opcode;
-        void (*f)(stack_t **stack, unsigned int line_number);
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
+/**
+ * struct strct_s - struct of info's structs
+ * @file: monty file
+ * @line: line file
+ * @stack: stack_t list
+ * @line_number: line number
+ * @state: int 0
+ * Description: global struct with program info
+ */
+typedef struct strct_s
+{
+	FILE *file;
+	char *line;
+	stack_t *stack;
+	unsigned int line_number;
+	int state;
+} strct_t;
+extern strct_t strct;
+
+/* prototypes */
+strct_t push(char *value, strct_t strct);
+void push1(char *value, strct_t strct);
+int _isdigit(char *str);
+void free_stck(stack_t *head);
+void lst_opcode(stack_t **stack, char *opcode, strct_t strct);
+void pall(stack_t **stack, unsigned int line_number);
+void pint(stack_t **stack, unsigned int line_number);
 
 #endif
