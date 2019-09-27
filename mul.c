7e02db324b4ee41c3e9fa0e2e6c881af2f3a716e
rolandoquiroz <rolandoquiroz@gmail.com>
2019-09-27 06:00:44 +0000

    mul.c added

---
+++ mul.c
@@ -0,0 +1,22 @@
+#include "monty.h"
+
+/**
+ * mul - mul
+ * @stack: head stack
+ * @line_number: line number
+ * Return: (void)
+ */
+void mul(stack_t **stack, unsigned int line_number)
+{
+	stack_t *current = *stack;
+	stack_t *next;
+
+	if (!current || !current->next)
+	{
+		dprintf(STDERR_FILENO, "L%u: can't mul, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	next = current->next;
+	next->n *= current->n;
+	pop(stack, line_number);
+}
