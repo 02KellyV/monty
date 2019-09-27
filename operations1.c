d69cb0b34578258f18a7923fe378a4a047f5c86e
rolandoquiroz <rolandoquiroz@gmail.com>
2019-09-27 04:55:32 +0000

    operations1.c readded

--- operations1.c
+++ operations1.c
@@ -1,30 +1,6 @@
 #include "monty.h"
 
 /**
- * pop - removes the top element of the stack
- * @stack: head stack
- * @line_number: line number
- * Return: (void)
- */
-void pop(stack_t **stack, unsigned int line_number)
-{
-	stack_t *current = *stack;
-	stack_t *next;
-
-	if (!current)
-	{
-		dprintf(STDERR_FILENO, "L%u: can't pop an empty stack\n", line_number);
-		exit(EXIT_FAILURE);
-	}
-	next = current->next;
-	free(current);
-	*stack = next;
-	current = *stack;
-	if (current)
-		current->prev = NULL;
-}
-
-/**
  * swap - swap two elements
  * @stack: head of stack
  * @line_number: line number
