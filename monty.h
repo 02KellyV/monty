9db37e14127def966d04c7a24a443811ae3f5ba5
rolandoquiroz <rolandoquiroz@gmail.com>
2019-09-27 06:32:03 +0000

    monty.h readded again

--- monty.h
+++ monty.h
@@ -73,7 +73,6 @@ void pop(stack_t **stack, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
 void add(stack_t **stack, unsigned int line_number);
 void sub(stack_t **stack, unsigned int line_number);
-void mul(stack_t **stack, unsigned int line_number);
 void lst_opcode(stack_t **stack, char *opcode, strct_t strct);
 
 #endif
